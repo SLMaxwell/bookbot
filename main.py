582c885d2c4039eed2d10f554cfbae3e956c08ef
Scott L. Maxwell <scott.maxwell@rammount.com>
2024-12-30 11:49:26 -0500

    B: Added Frankenstein and show book.

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("hello world")
+def main() -> int:
+  path_to_file = "./books/frankenstein.txt"
+
+  with open(path_to_file) as f:
+    file_contents = f.read()
+
+  print(file_contents)
+
+if __name__ == '__main__':
+  main()  # next section explains the use of sys.exit
